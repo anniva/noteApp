134ba4dec6259b0e54620a2ae4438e8f85581333
Anni Vaananen <anni.vaananen@gmail.com>
2015-08-07 12:51:01 +0100

    mongod

--- api/handlers/mongo_handlers.js
+++ api/handlers/mongo_handlers.js
@@ -5,6 +5,7 @@ var url = 'mongodb://localhost:27017/notes',
 
 module.exports = {
  createNote:createNote,
+ getNotes:getNotes,
 };
 
 
@@ -22,3 +23,59 @@ function createNote (id, msg, callback) {
 }
 
 
+function getNotes (id, callback) {
+  MongoClient.connect(url, function (err, db){
+    var notes = db.collection('notes');
+    notes.find({_id: id},
+      function (err, result) {
+        db.close();
+        /* istanbul ignore if */    
+        if (err) { callback(err); } 
+        else { callback(null, result); }
+      });
+  });
+}
+
+
+
+/*
+function getNotes (input, callback) {
+  MongoClient.connect(url, function (err, db){
+    var notes = db.collection('notes');
+    notes.find(input,
+      function (err, result) {
+        db.close();
+        console.log(result);
+        if (err) { callback(err); } 
+        else { callback(null, result); }
+        console.log(result);
+      });
+  });
+}
+
+/*
+function getNotes (input, callback) {
+  MongoClient.connect(url, function (err, db){
+    var notes = db.collection('notes');
+    notes.find(input,
+      function (err, result) {
+        db.close();
+        console.log(result);
+        if (err) { callback(err); } 
+        else { callback(null, result); }
+      });
+  });
+}
+function getNotes (input, callback) {
+  MongoClient.connect(url, function (err, db){
+      db.getCollection(input, function(err, result) {
+      db.close();
+      if (err) { callback(err); } 
+      else { callback(null, result); }
+    });
+  });
+}
+
+*/
+
+        /* istanbul ignore if */    
--- test/_mongo_tests.js
+++ test/_mongo_tests.js
@@ -9,9 +9,37 @@ test('mongo createnote function works', function(t) {
   var id = Date.now();
   var message= 'test message';
   mongo.createNote(id, message, function (err, result) {
-    console.log(result.ops);
+    console.log(result);
     t.deepEqual(result.ops, [{_id: id, note:message}]);
     t.end();
   });
 });
 
+
+test('mongo getNotes function works', function(t) {
+  var id = 1438876897441 ;
+  //var message= 'test message';
+  mongo.getNotes(id,function (err, result) {
+    //console.log(result);
+    //t.deepEqual(result.ops, [{_id: id, note:message}]);
+    t.end();
+  });
+});
+
+
+
+
+//1438876897441 (id)
+
+/*
+test('mongo getNotes function works', function(t) {
+  var collection = {};
+  mongo.getNotes(collection , function (err, result) {
+    //console.log(result);
+    //t.notEqual(result.ops.length,0);
+    
+    t.end();
+  });
+});
+
+*/

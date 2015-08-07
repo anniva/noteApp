50fb39ead08f22fd2bd5f5501108a15e23fec683
Anni Vaananen <anni.vaananen@gmail.com>
2015-08-07 18:24:04 +0100

    fixed test

--- test/_landing_test.js
+++ test/_landing_test.js
@@ -1,6 +1,10 @@
 var test = require('tape');
 var server = require('../api/server.js');
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/notes';
+var mongo = require('../api/handlers/mongo_handlers');
+
 
 
 test('/landing route  should return landing page as expected ', function(t) {
@@ -10,11 +14,20 @@ test('/landing route  should return landing page as expected ', function(t) {
   }); 
 });
 
+
+
+test('mongo createnote function works', function(t) {
+  var id = Date.now();
+  var message= 'qwertyuiop';
+  mongo.createNote(id, message, function (err, result) {
+    t.deepEqual(result.ops, [{_id: id, note:message}]);
+    t.end();
+  });
+});
+
 test('/landing route  renders the testmessage on page', function(t) {
   server.inject({method:'GET', url:'/landing'}, function(response, reply) {
     t.equal(isInBody(response.result, 'qwertyuiop'), true);
-    //t.equal(isInBody(response.result, 'Cheer Up Claire!'), true);
-    
     t.end();
   }); 
 });

f3337decad63321c48ceb2d1513ff34618e1373c
Anni Vaananen <anni.vaananen@gmail.com>
2015-09-01 10:19:52 +0100

    empty note won't call createnote & will be redirected back to landing

--- api/handlers/note_handler.js
+++ api/handlers/note_handler.js
@@ -3,10 +3,13 @@ var mongo = require('./mongo_handlers.js');
 function note_handler (request, reply){
   var id = Date.now();
   var note = request.url.query.content;
-    mongo.createNote(id, note, function(err, result) {
-      //console.log(result);
+  if(note.length>0){
+    mongo.createNote(id, note, function(err, result) { 
       reply.redirect('/landing');
     });
+  }else{
+    reply.redirect('/landing');
+  }
 }
 
 
--- test/_note_handler_test.js
+++ test/_note_handler_test.js
@@ -3,14 +3,21 @@ var server = require('../api/server.js');
 
 
 test('request contains note from form', function(t) {
-  var test='qwertyuiop';
-  server.inject({method:'GET', url:'/note?content='+test}, function(response) {
+  var teststring='qwertyuiop';
+  server.inject({method:'GET', url:'/note?content='+teststring}, function(response) {
     //console.log(response);
-    t.equal(containsNote(response.raw.req.url, test), true);
+    t.equal(containsNote(response.raw.req.url, teststring), true);
     t.end(); 
   });
 });
 
+test('If note == emptystring will be redirect [statusCode 302]', function(t) {
+  var emptystring='';
+  server.inject({method:'GET', url:'/note?content='+emptystring}, function(response) {
+    t.equal(response.statusCode, 302);
+    t.end(); 
+  });
+});
 
 
 function containsNote (x, y) {
